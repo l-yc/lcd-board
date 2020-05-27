872ccc16639bbbf3b095330ab1626bcd9e5b5d48
lyc <22719973+l-yc@users.noreply.github.com>
2020-05-27 12:01:51 +0800

    refactored socket server code to improve modularity and readability

--- src/Socket.ts
+++ src/Socket.ts
@@ -1,89 +1,133 @@
-import socketio from 'socket.io';
+import SocketIO from 'socket.io';
 
 interface User {
   username: string | null;
   room: string | null;
 }
 
-class Socket {
-  public io: any;
+interface Room {
+  users: string[];
+}
 
-  private userTracker: Map<string,User>;
-  private roomTracker: any;
+// wrapper around SocketIO.Socket
+class Socket {
+  private socket: SocketIO.Socket;
+  private server: SocketServer;
+
+  private room: string | null; // FIXME some redundancy here with server, maybe should think of another way
+
+  constructor(socket: SocketIO.Socket, server: SocketServer) {
+    this.socket = socket;
+    this.server = server;
+
+    this.room = null;
+
+    socket.on('disconnect', this.onDisconnect.bind(this));
+    socket.on('register', this.onRegister.bind(this));
+    socket.on('join', this.onJoin.bind(this));
+    socket.on('leave', this.onLeave.bind(this));
+    socket.on('draw event', this.onDrawEvent.bind(this));
+  }
+
+  private onDisconnect(): void {
+    console.log('%s disconnected', this.socket.id);
+  }
+
+  private onRegister(username: string): void {
+    console.log('%s registered as %s', this.socket.id, username);
+    this.server.registerUser(this.socket.id, username);
+  }
+
+  private onJoin(room: string): void {
+    if (this.room) {
+      this.socket.leave(this.room);
+    }
+    console.log('%s joined %s', this.socket.id, room);
+    this.socket.join(room);
+    this.room = room;
+    this.server.setUserRoom(this.socket.id, room);
+  }
+
+  private onLeave(): void {
+    if (this.room) {
+      this.socket.leave(this.room);
+      this.server.setUserRoom(this.socket.id, null);
+    }
+  }
+
+  private onDrawEvent(event: any): void {
+    console.log('received event %o', event);
+    if (!this.room) {
+      // user is not subscribed to any room, don't broadcast
+      console.log('invalid event: the user ' + this.socket.id + ' is not subscribed to any room!');
+      return;
+    } 
+    this.socket.broadcast
+      .to(this.room).emit('draw event', {'event' : event, 'originUserId': this.socket.id});
+  }
+};
+
+class SocketServer {
+  public io: SocketIO.Server;
+
+  private sockets: Map<string,Socket>;
+  private users: Map<string,User>;
+  private rooms: Map<string,Room>;
 
   constructor(http: any) {
-    this.io = socketio(http);
+    this.io = SocketIO(http);
 
-    this.userTracker = new Map();
-    this.roomTracker = {};
+    this.sockets = new Map();
+    this.users = new Map();
+    this.rooms = new Map();
 
     this.mountListeners(this);
   }
 
-  private mountListeners(data: Socket): void {
-    function leaveRoom(socket: any, room: string): void {
-      socket.leave(room);
-      let user = data.userTracker.get(socket.id)
-      if (user) user.room = null;
-      let idx = data.roomTracker[room].users.indexOf(socket.id);
-      data.roomTracker[room].users.splice(idx, 1);
-    };
-
-    this.io.on('connection', (socket: any) => {
+  private mountListeners(data: SocketServer): void {
+    this.io.on('connection', (socket: SocketIO.Socket) => {
       console.log('%s connected', socket.id);
-
-      socket.on('disconnect', () => {
-        console.log('%s disconnected', socket.id);
-      });
-
-      socket.on('register', (username: string) => {
-        console.log('%s registered as %s', socket.id, username);
-        if (!data.userTracker.has(socket.id)) {
-          data.userTracker.set(socket.id, { username: null, room: null });
-        }
-        let user = data.userTracker.get(socket.id)
-        if (user) user.username = username;
-      });
-        
-      socket.on('join', (room: string) => {
-        if (!data.userTracker.has(socket.id)) return;
-        console.log('%s joined %s', socket.id, room);
-        socket.join(room);
-
-        let user = data.userTracker.get(socket.id) as User;
-        if (user.room) {
-          leaveRoom(socket, room);
-        };
-        user.room = room;
-
-        if (!data.roomTracker[room]) {
-          data.roomTracker[room] = { users: [] };
-        }
-        data.roomTracker[room].users.push(socket.id);
-
-        data.io.to(room).emit('room info', data.roomTracker[room].users);
-      });
-
-      socket.on('leave', () => {
-        let room = data.userTracker.get(socket.id)?.room as string | null;
-        if (room) {
-          leaveRoom(socket, room);
-          data.io.to(room).emit('room info', data.roomTracker[room].users);
-        }
-      });
-
-      socket.on('draw event', (event: any) => {
-        console.log('received event %o', event);
-        let room = data.userTracker.get(socket.id)?.room;
-        if (!room) {
-          // user is not subscribed to any room, don't broadcast
-          console.log('invalid event: the user ' + socket.id + ' is not subscribed to any room!');
-          return;
-        } 
-        socket.broadcast.to(room).emit('draw event', {'event' : event, 'originUserId': socket.id});
-      });
+      this.sockets.set(socket.id, new Socket(socket, this));
     });
+  }
+
+  public registerUser(socketId: string, username: string): void {
+    if (!this.users.has(socketId)) {
+      this.users.set(socketId, { username: null, room: null });
+    }
+    let user = this.users.get(socketId) as User;
+    user.username = username;
+  }
+
+  public setUserRoom(socketId: string, room: string | null): void {
+    let user = this.users.get(socketId) as User;
+
+    // leave
+    if (user.room) {
+      let r = this.rooms.get(user.room) as Room;
+      let idx = r.users.indexOf(socketId);
+      r.users.splice(idx, 1);
+
+      this.io.to(user.room).emit('room info', r.users);
+    }
+
+    // join
+    if (room) {
+      let r: Room;
+      if (!this.rooms.has(room)) {
+        r = { users: [] };
+        this.rooms.set(room, r);
+      } else {
+         r = this.rooms.get(room) as Room;
+      }
+      r.users.push(socketId);
+
+      this.io.to(room).emit('room info', r.users);
+    }
+
+    // update user
+    user.room = room;
   }
 };
 
-export default Socket;
+export default SocketServer;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import http from 'http';
 
 import app from './App';
-import Socket from './Socket';
+import SocketServer from './Socket';
 
 let config = require('../config/main'); // config file is not a module
 
 let httpServer = new http.Server(app);
-let socketServer = new Socket(httpServer);
+let socketServer = new SocketServer(httpServer);
 
 const port = process.env.PORT || config.port;
 const server = httpServer.listen(port, function() {
