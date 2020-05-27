37dcc584f6e52413644ead7dd49fd963fb670818
Wern Jie Lim <lwjdev@gmail.com>
2020-05-27 19:56:45 +0800

    added pressure sensitivity support

--- src/public/js/index.ts
+++ src/public/js/index.ts
@@ -102,7 +102,7 @@ class SocketServer {
     this.socket.emit('join', room);
   }
 
-  sendDrawEvent(event: DrawEvent, pathId: number) {
+  sendDrawEvent(event: DrawEvent, pathId: string) {
     if (!this.room) return; // not initialised, FIXME throw an error
 
     let group: string = `${this.getUserId()}_${pathId}`; // globally unique id
@@ -122,6 +122,8 @@ class SocketServer {
 class DrawingTool {
   private tool: paper.Tool;
   private path: paper.Path | null;
+  private pathsDrawnCount = 0;
+  readonly toolId: string;
 
   public channel: SocketServer | null;
 
@@ -129,8 +131,7 @@ class DrawingTool {
   readonly name: string;
   public size: number = 2;
 
-
-  public constructor(name: string) {
+  public constructor(name: string, id: string) {
     // Create a simple drawing tool:
     let tool = new paper.Tool();
 
@@ -142,6 +143,7 @@ class DrawingTool {
     tool.onMouseUp   = this.handleMouseEvent.bind(this);
 
     this.name = name;
+    this.toolId = id;
     this.path = null;
     this.channel = null;
   }
@@ -156,34 +158,91 @@ class DrawingTool {
       default: return;
     }
     this.handle({
-        action: action,
-        point: {
-            x: event.point.x,
-            y: event.point.y,
-        },
-        color: activeColor,
-        size: this.size
+      action: action,
+      point: {
+        x: event.point.x,
+        y: event.point.y,
+      },
+      color: activeColor,
+      size: this.size + Math.min(this.size, 10) * (this.sizeAdjustmentFactor - 1)
     });
   }
 
-  public handle(event: DrawEvent) {
-    log('handling', event);
+  private sizeAdjustmentFactor: number = 1;
+  public interceptPressureEventsOnCanvas(canvas: HTMLElement) {
+    if (canvas) {
+      let pointerEventPressureHandler = (event: any) => {
+        if (event.buttons && event.pressure) {
+          this.sizeAdjustmentFactor = event.pressure + 0.5;
+        }
+      }
 
-    if (event.action == "begin" || this.path == null) {
-      this.path = new paper.Path();
-    }
+      let webkitForcePressureEventHandler = (event: any) => {
+        //handles apple's proprietary force touch api
+        if (event.webkitForce) {
+          this.sizeAdjustmentFactor = Math.min(0.5 + Math.max(0, event.webkitForce - 0.75), 1.5);
+        }
+      }
 
-    this.path.strokeColor = new paper.Color(event.color);
-    this.path.strokeWidth = event.size;
+      let endEventHandler = (event: any) => {
+        this.sizeAdjustmentFactor = 1;
+      }
 
-    if (event.point) this.path.add(new paper.Point(event.point));
-
-    if (this.channel && !event.originUserId) {
-      this.channel.sendDrawEvent(event, this.path.id);
+      canvas.addEventListener("webkitmouseforcechanged", webkitForcePressureEventHandler);
+      canvas.addEventListener('pointerdown', pointerEventPressureHandler);
+      canvas.addEventListener('pointermove', pointerEventPressureHandler);
+      canvas.addEventListener('pointerup', endEventHandler);
     }
   }
 
-  activate() {
+  private previousDrawEvent: DrawEvent | null = null;
+  public handle(event: DrawEvent) {
+    log('handling', event);
+
+    if (event.action == "end") {
+      // we do some cleanup when a draw action ends
+      this.path = null;
+      this.previousDrawEvent = null;
+      this.sizeAdjustmentFactor = 1;
+    } else {
+      // handle draw action
+      // process and handle all properties
+      let color = new paper.Color(event.color);
+      let size = event.size;
+
+      let firstEventCall = event.action == "begin";
+      let strokePropertiesChanged = this.path != null && (this.path.strokeWidth != size || this.path.strokeColor != color);
+
+      if (this.path == null || firstEventCall || strokePropertiesChanged) {
+        this.path = new paper.Path();
+        this.path.strokeCap = 'round';
+      }
+
+      // apply settings
+      this.path.strokeColor = color;
+      this.path.strokeWidth = size;
+
+      // increment internal paths drawn counter for each stroke
+      if (firstEventCall) this.pathsDrawnCount += 1;
+
+      // if this stroke is just due to a property change rather than a new stroke, we connect it with the previous stroke.
+      if (strokePropertiesChanged && this.previousDrawEvent) {
+        this.path.add(new paper.Point(this.previousDrawEvent.point));
+      }
+
+      // we add a new point for the current location
+      this.path.add(new paper.Point(event.point));
+      this.previousDrawEvent = event;
+    }
+
+    // broadcast draw event to others if required
+    if (this.channel && !event.originUserId) {
+      this.channel.sendDrawEvent(event, this.toolId + "_" + this.pathsDrawnCount);
+    }
+
+  }
+
+  public activate() {
     this.tool.activate();
   }
 };
@@ -196,11 +255,12 @@ class DrawingMember {
   constructor(id: string, user: User) {
     this.id = id;
     this.user = user;
-    this.drawingTool = new DrawingTool(id);
+    this.drawingTool = new DrawingTool(user.username || id, id);
   }
 
   handle(event: DrawEvent) {
-    this.drawingTool.handle(event)
+    if (event.originUserId == this.id)
+      this.drawingTool.handle(event)
   }
 }
 
@@ -233,10 +293,16 @@ function setActiveColor(color: string) {
 
 
 window.onload = () => {
+  let canvas = document.getElementById('myCanvas');
+  if (!canvas) {
+    log("something's terribly wrong: the canvas is missing. aborting operations.");
+    return;
+  }
+
   paper.setup('myCanvas');
 
   drawingTools = [
-    new DrawingTool('Pen'),
+    new DrawingTool('Pen', 'PEN'),
   ];
 
   drawingColors = [
@@ -252,8 +318,8 @@ window.onload = () => {
 
   let toolPickerContainer = document.getElementById('tool-picker-container');
   if (toolPickerContainer) {
-    for (i in drawingTools) {
-      let tool = drawingTools[i];
+    for (let tool of drawingTools) {
+      tool.interceptPressureEventsOnCanvas(canvas);
       let button = document.createElement("button");
       button.innerText = tool.name;
       button.classList.add('toolOption');
@@ -265,8 +331,7 @@ window.onload = () => {
   }
   let colorPickerContainer = document.getElementById('color-picker-container');
   if (colorPickerContainer) {
-    for (var i in drawingColors) {
-      const color = drawingColors[i];
+    for (let color of drawingColors) {
       const button = document.createElement("button");
       button.style.backgroundColor = color;;
       button.classList.add('colorOption');
@@ -276,6 +341,7 @@ window.onload = () => {
       colorPickerContainer.appendChild(button);
     }
   }
+
 
   ui = new UI();
   socketServer = new SocketServer();
